6c625f7e14341942c65e0f3187a11cf545c9d5b4
IraKalinka <ira.boeing@gmail.com>
2023-06-25 00:41:46 -0700

    try1

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -3,5 +3,7 @@
 describe('first Test', () => {
   it('visit to project', () => {
     cy.visit('https://automationteststore.com/')
-  })
+    cy.get('a.menu_home').realHover().click();
+  });
+
 });
